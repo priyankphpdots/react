646efcba6edd05bb9f2d1d73786832809cfa4df9
sandip109 <114212167+sandip109@users.noreply.github.com>
2022-11-21 17:54:31 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ClassCounter from './components/ClassCounter'
+import ComponentC from './components/ComponentC';
+import DataFetching from './components/DataFetching';
+import DataFetchingId from './components/DataFetchingId';
+import HookCounter from './components/HookCounter';
+import HookCounterFive from './components/HookCounterFive';
+import HookCounterFour from './components/HookCounterFour';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterTwo from './components/HookCounterTwo';
+import HookMouse from './components/HookMouse';
+import IntervalHookCounter from './components/IntervalHookCounter';
+import MouseContainer from './components/MouseContainer';
+
+ export const UserContext = React.createContext()
+ export const ChannelContext = React.createContext()
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ClassCounter /> */}
+       {/* <HookCounter />  */}
+       {/* <HookCounterTwo />  */}
+       {/* <HookCounterThree />  */}
+       {/* <HookCounterFour /> */}
+       {/* <HookCounterFive />  */}
+       {/* <HookMouse />  */}
+       {/* <MouseContainer />  */}
+      {/* <IntervalHookCounter />  */}
+     {/* <DataFetching />  */}
+       {/* <DataFetchingId /> */}
+        
+      <UserContext.Provider value={'Priyank'}>
+      <ChannelContext.Provider value={'Gondaliya'}>
+      <ComponentC />
+      </ ChannelContext.Provider>
+      </ UserContext.Provider>
+
     </div>
   );
 }
